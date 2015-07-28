029d610b382ae64bce5223dea4105d2c5e239b9b
elfanek <omar.elfanek@gmail.com>
2015-07-28 14:55:23 -0400

    Xcode base project

---
+++ Icebreaker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Icebreaker
+//
+//  Created by Omar El-Fanek on 7/28/15.
+//  Copyright (c) 2015 ChickenBiscut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Icebreaker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Icebreaker
+//
+//  Created by Omar El-Fanek on 7/28/15.
+//  Copyright (c) 2015 ChickenBiscut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
