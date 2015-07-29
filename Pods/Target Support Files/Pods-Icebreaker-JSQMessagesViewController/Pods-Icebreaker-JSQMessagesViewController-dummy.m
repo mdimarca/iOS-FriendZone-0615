2d9abf730abd356ff0c06dad4a54ba84ebcb590e
elfanek <omar.elfanek@gmail.com>
2015-07-29 17:56:59 -0400

    removed Pods from git cache

--- Pods/Target Support Files/Pods-Icebreaker-JSQMessagesViewController/Pods-Icebreaker-JSQMessagesViewController-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Icebreaker_JSQMessagesViewController : NSObject
-@end
-@implementation PodsDummy_Pods_Icebreaker_JSQMessagesViewController
-@end
