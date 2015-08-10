1610be20b1043d63aff00a4cf1aae08a9e2172a4
elfanek <omar.elfanek@gmail.com>
2015-08-10 14:36:21 -0400

    Create game class

---
+++ Icebreaker/Game.h
@@ -0,0 +1,13 @@
+//
+//  Game.h
+//  Icebreaker
+//
+//  Created by Omar El-Fanek on 8/10/15.
+//  Copyright (c) 2015 ChickenBiscut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Game : UIViewController
+
+@end
---
+++ Icebreaker/Game.m
@@ -0,0 +1,13 @@
+//
+//  Game.m
+//  Icebreaker
+//
+//  Created by Omar El-Fanek on 8/10/15.
+//  Copyright (c) 2015 ChickenBiscut. All rights reserved.
+//
+
+#import "Game.h"
+
+@implementation Game
+
+@end
