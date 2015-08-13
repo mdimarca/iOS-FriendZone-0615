14862f9adbcc007331894a46030326fd694ef120
ganchau <gancchau@gmail.com>
2015-08-13 11:55:26 -0400

    added match blur feature

--- Icebreaker/friendSwiperViewController.m
+++ Icebreaker/friendSwiperViewController.m
@@ -12,13 +12,26 @@
 #import "ChoosePersonViewOurs.h"
 
 static const CGFloat ChoosePersonButtonHorizontalPadding = 88.f;
-static const CGFloat ChoosePersonButtonHVerticalPadding = 20.f;
+static const CGFloat ChoosePersonButtonVerticalPadding = 20.f;
 
 @interface friendSwiperViewController () < MDCSwipeToChooseDelegate>
 
 @property (strong, nonatomic) DataStore *dataManager;
 @property (strong, nonatomic) NSMutableArray *potentialMatches;
 @property (nonatomic, strong) NSMutableArray *trackPotentialMatches;
+@property (weak, nonatomic) IBOutlet UIImageView *leftImage;
+@property (weak, nonatomic) IBOutlet UIImageView *rightImage;
+@property (weak, nonatomic) IBOutlet UIView *matchView;
+
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *leftProfilePictureConstraint;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *rightProfilePictureConstraint;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *matchNotificationLabelTop;
+
+@property (weak, nonatomic) IBOutlet UILabel *itsAMatchLabel;
+@property (strong, nonatomic) IBOutlet UIView *viewProperty;
+
+@property (strong, nonatomic) UIButton *likeButton;
+@property (strong, nonatomic) UIButton *rejectButton;
 
 @end
 
@@ -36,19 +49,11 @@ static const CGFloat ChoosePersonButtonHVerticalPadding = 20.f;
     
     NSLog(@"LOCAL USER2 %@",self.dataManager.user.facebookID);
     
-    
-
     [ParseAPICalls getMatchesFromParseWithCompletionBlock:^(BOOL success, NSArray *matches) {
-        
         if (success) {
-            
             NSLog(@"WWOWOWOOWOWOWOWOWOWOWOWO!!! : %@", matches);
         }
-        
-        
     }];
-
-    
 
     [self.dataManager fetchMatchesWithCompletionBlock:^(BOOL success) {
         if (success) {
@@ -64,13 +69,95 @@ static const CGFloat ChoosePersonButtonHVerticalPadding = 20.f;
             
             //update UI stuff
         } else {
-            
-        
             //failure, alert user of failure
         }
-        
-        
     }];
+    self.viewProperty.backgroundColor = [UIColor colorWithRed:249/255.0 green:249/255.0 blue:249/255.0 alpha:1.0];
+    self.matchView.hidden = YES;
+    self.itsAMatchLabel.hidden = YES;
+    
+    
+    self.leftProfilePictureConstraint.constant -= 160.0;
+    self.rightProfilePictureConstraint.constant -= 160.0;
+    self.matchNotificationLabelTop.constant += 40.0;
+    
+    self.leftImage.autoresizingMask = NO;
+    self.rightImage.autoresizingMask = NO;
+    self.itsAMatchLabel.autoresizingMask = NO;
+    
+    [self constructNopeButton];
+    [self constructLikedButton];
+}
+
+-(void)displayMatchNotification:(User *)otherUser{
+    NSLog(@"GETTING CALLED");
+    PFUser *localUser = [PFUser currentUser];
+    UIImage *profilePhoto = [UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:localUser[@"profile_photo"]]]];
+    
+    self.leftImage.image = profilePhoto;
+    self.rightImage.image = otherUser.profilePhoto;
+    [self animateMatchPictures];
+}
+
+-(void)animateMatchPictures{
+    UIBlurEffect *blur = [UIBlurEffect effectWithStyle:UIBlurEffectStyleLight];
+    UIVisualEffectView *effectView = [[UIVisualEffectView alloc] initWithEffect:blur];
+    effectView.frame = self.view.frame;
+    effectView.alpha = 0.5;
+    [self.view addSubview:effectView];
+    
+    self.leftImage.layer.cornerRadius = 50;
+    self.rightImage.layer.cornerRadius = 50;
+    self.leftImage.layer.masksToBounds = YES;
+    self.rightImage.layer.masksToBounds = YES;
+    [self.view bringSubviewToFront:self.matchView];
+    
+    self.matchView.hidden = NO;
+    
+    self.rightImage.hidden = NO;
+    self.leftImage.hidden = NO;
+    self.rightImage.alpha = 1.0;
+    self.leftImage.alpha = 1.0;
+    self.itsAMatchLabel.hidden = NO;
+    self.itsAMatchLabel.alpha = 1.0;
+    
+    [UIView animateWithDuration:1.3 animations:^{
+        self.leftProfilePictureConstraint.constant += 160.0;
+        self.rightProfilePictureConstraint.constant += 160.0;
+        self.matchNotificationLabelTop.constant -= 40.0;
+        effectView.alpha = 1;
+        [self.view layoutIfNeeded];
+        
+    } completion:^(BOOL finished) {
+        [UIView animateWithDuration:0.6 animations:^{
+            self.rightImage.alpha = 0.0;
+            self.leftImage.alpha = 0.0;
+            self.itsAMatchLabel.alpha = 0.0;
+            effectView.alpha = 0.1;
+            
+            NSLog(@"ANIMATE");
+        } completion:^(BOOL finished) {
+            self.matchView.hidden = YES;
+            self.leftProfilePictureConstraint.constant -= 160.0;
+            self.rightProfilePictureConstraint.constant -= 160.0;
+            self.matchNotificationLabelTop.constant += 40.0;
+            self.rightImage.hidden = YES;
+            self.leftImage.hidden = YES;
+            [effectView removeFromSuperview];
+        }];
+    }];
+}
+
+-(void)keepOrRemoveLikeAndRejectButton{
+    DataStore *dataManager = [DataStore sharedDataStore];
+    //hides the like and reject buttons if there isnt anyone to show
+    if (dataManager.potentialMatchArray.count == 0 ||
+        dataManager.potentialMatchArray == nil) {
+        //        self.rejectButton.hidden = YES;
+        //        self.likeButton.hidden = YES;
+        [self.rejectButton removeFromSuperview];
+        [self.likeButton removeFromSuperview];
+    }
 }
 
 #pragma mark - MDCSwipeToChooseDelegate Callbacks
@@ -83,8 +170,10 @@ static const CGFloat ChoosePersonButtonHVerticalPadding = 20.f;
 // This is called then a user swipes the view fully left or right.
 - (void)view:(UIView *)view wasChosenWithDirection:(MDCSwipeDirection)direction {
     DataStore *dataManager = self.dataManager;
+    [self keepOrRemoveLikeAndRejectButton];
 
-     User *user = self.trackPotentialMatches[0];
+    User *user = self.trackPotentialMatches[0];
+    [self displayMatchNotification:user];
     
     if (direction == MDCSwipeDirectionLeft) {
         NSLog(@"Photo rejected!");
@@ -158,7 +247,62 @@ static const CGFloat ChoosePersonButtonHVerticalPadding = 20.f;
     
 }
 
+#pragma mark - button creation
 
+// Create and add the "nope" button.
+- (void)constructNopeButton {
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    UIImage *image = [UIImage imageNamed:@"nope@2x.png"];
+    button.frame = CGRectMake(ChoosePersonButtonHorizontalPadding,
+                              CGRectGetMaxY(self.backCardView.frame) + ChoosePersonButtonVerticalPadding + 450.f,
+                              image.size.width,
+                              image.size.height);
+    [button setImage:image forState:UIControlStateNormal];
+    [button setTintColor:[UIColor colorWithRed:247.f/255.f
+                                         green:91.f/255.f
+                                          blue:37.f/255.f
+                                         alpha:1.f]];
+    [button addTarget:self
+               action:@selector(nopeFrontCardView)
+     forControlEvents:UIControlEventTouchUpInside];
+    
+    self.rejectButton = button;
+    
+    [self.view addSubview:button];
+}
 
+// Create and add the "like" button.
+- (void)constructLikedButton {
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    UIImage *image = [UIImage imageNamed:@"liked@2x.png"];
+    button.frame = CGRectMake(CGRectGetMaxX(self.view.frame) - image.size.width - ChoosePersonButtonHorizontalPadding,
+                              CGRectGetMaxY(self.backCardView.frame) + ChoosePersonButtonVerticalPadding + 450.f,
+                              image.size.width,
+                              image.size.height);
+    [button setImage:image forState:UIControlStateNormal];
+    [button setTintColor:[UIColor colorWithRed:29.f/255.f
+                                         green:245.f/255.f
+                                          blue:106.f/255.f
+                                         alpha:1.f]];
+    [button addTarget:self
+               action:@selector(likeFrontCardView)
+     forControlEvents:UIControlEventTouchUpInside];
+    
+    self.likeButton = button;
+    
+    [self.view addSubview:button];
+}
+
+#pragma mark Control Events
+
+// Programmatically "nopes" the front card view.
+- (void)nopeFrontCardView {
+    [self.frontCardView mdc_swipe:MDCSwipeDirectionLeft];
+}
+
+// Programmatically "likes" the front card view.
+- (void)likeFrontCardView {
+    [self.frontCardView mdc_swipe:MDCSwipeDirectionRight];
+}
 
 @end
